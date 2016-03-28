53bed9f3c92fb2342f1d5a9c03f77b219508d1c1
pablok47 <pkaparis@gmail.com>
2016-03-28 17:46:06 -0400

    added notes and comments to the files

--- public/js/main.js
+++ public/js/main.js
@@ -19166,6 +19166,7 @@ var React = require('react');
 var ReactDOM = require('react-dom'); //react-dom renders items to the screen
 var ListManager = require('./components/ListManager.jsx');
 
+//copy and pasted the top line below to create more lists
 ReactDOM.render(React.createElement(ListManager, { title: 'Stock Manager' }), document.getElementById('stocks'));
 ReactDOM.render(React.createElement(ListManager, { title: 'ETF Manager' }), document.getElementById('etfs'));
 ReactDOM.render(React.createElement(ListManager, { title: 'Fund Manager' }), document.getElementById('funds'));
--- source/main.jsx
+++ source/main.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var ReactDOM = require('react-dom'); //react-dom renders items to the screen
 var ListManager = require('./components/ListManager.jsx');
 
+//copy and pasted the top line below to create more lists
 ReactDOM.render(<ListManager title="Stock Manager" />, document.getElementById('stocks'));
 ReactDOM.render(<ListManager title="ETF Manager" />, document.getElementById('etfs'));
 ReactDOM.render(<ListManager title="Fund Manager" />, document.getElementById('funds'));
