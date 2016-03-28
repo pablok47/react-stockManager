8edbdc5227533511f9fb3cbb1d643c113e958510
pablok47 <pkaparis@gmail.com>
2016-03-28 15:09:16 -0400

    included links to the index.html file plus provided some initial styling in the ListManager.jsx file

--- source/components/ListManager.jsx
+++ source/components/ListManager.jsx
@@ -7,29 +7,42 @@ var ListManager = React.createClass({
         return {items: [], newItemText: ''};
     },
     onChange: function(e) {
+        //update the state property every time a keystroke is typed
         this.setState({newItemText: e.target.value});
     },
     handleSubmit: function(e) {
+        //stop the button from getting clicks since we are using onSubmit
         e.preventDefault();
 
+        //grabs the current list of items
         var currentItems = this.state.items;
 
+        //adds new item to the list
         currentItems.push(this.state.newItemText);
 
+        //updates the main item list with the new list and clears the newItemText
         this.setState({items: currentItems, newItemText: ''});
 
     },
 
     render: function() {
+        //onChange is called with every keystroke so we can store the most recent data entered
+        //value is what he user sees on the input-box...we point this to newItemText so it updates on every keystroke
+        //below use className instead of just class because react confused it with createClass
         return (
-          <div>
-              <h3>{this.props.title}</h3>
-              <form onSubmit={this.handleSubmit}>
-                  <input onChange={this.onChange} value={this.state.newItemText} />
-                  <button>Add</button>
-              </form>
-              <List items={this.state.items} />
-
+          <div className="col-sm-4">
+              <div className="panel panel-default">
+                  <div className="panel-heading">
+                      <h3>{this.props.title}</h3>
+                  </div>
+                  <div className="panel-body">
+                      <form onSubmit={this.handleSubmit}>
+                          <input onChange={this.onChange} value={this.state.newItemText} />
+                          <button>Add</button>
+                      </form>
+                      <List items={this.state.items} />
+                  </div>
+              </div>
           </div>
         );
     }
