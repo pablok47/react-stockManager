2be67da3cdfa46b115df0932c5baab6891794426
pablok47 <pkaparis@gmail.com>
2016-04-08 16:45:25 -0400

    Initial commit for Page1 and Page2 jsx files

--- source/components/Base.jsx
+++ source/components/Base.jsx
@@ -1,1 +1,11 @@
 var React = require('react');
+
+var Base = React.createClass({
+    render: function() {
+        <div>
+            <h1>Header</h1>
+            {this.props.children}
+            <h1>Footer</h1>
+        </div>
+    }
+});
