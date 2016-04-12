acf9b48f61664aa16dac91ac82e9170155bc6919
pablok47 <pkaparis@gmail.com>
2016-04-12 18:15:31 -0400

    added code for the Email Field which includesa validator to see if the email is valid; added the element to the main.jsx file and to the index.html so that it appears on the app

--- source/components/EmailField.jsx
+++ source/components/EmailField.jsx
@@ -1,13 +1,25 @@
 var React = require('react');
+var validator = require('email-validator');
 
 var EmailField = React.createClass({
-    onChange: function(e) {
+    getInitialState: function() {
+        return {valid: true, value: ""}
+    },
 
+    onChange: function(e) {
+        var val = e.target.value;
+
+        if (!validator(e.target.value)) {
+          this.setState({valid: false, value: e.target.value})
+        } else {
+          this.setState({valid: true, value: e.target.value});
+        }
     },
     render: function() {
+        var formClass = this.state.valid ? "form-group" : "form-group has-error"
         return (
-            <div className="form-group">
-                <input className="form-control" onChange={this.onChange} placeholder="Email"></input>
+            <div className={formClass}>
+                <input className="form-control" onChange={this.onChange} placeholder="Email" value={this.state.value}></input>
             </div>
         );
     }
--- source/main.jsx
+++ source/main.jsx
@@ -3,12 +3,14 @@ var ReactDOM = require('react-dom'); //react-dom renders items to the screen
 var ListManager = require('./components/ListManager.jsx');
 var Clock = require('./components/Clock.jsx');
 var Notes = require('./components/Notes.jsx');
+var EmailField = require('./components/EmailField.jsx');
 
 var Routes = require('./Routes.jsx');
 
 //copy and pasted the top line below to create more lists
 ReactDOM.render(Routes, document.getElementById('main'));
 
+ReactDOM.render(<EmailField />, document.getElementById('email'));
 
 ReactDOM.render(<ListManager title="Stocks List" />, document.getElementById('stocks'));
 ReactDOM.render(<ListManager title="ETFs List" />, document.getElementById('etfs'));
