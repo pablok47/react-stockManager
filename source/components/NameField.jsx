02388acdd3846009df753212b6ed1b132c0f91a7
pablok47 <pkaparis@gmail.com>
2016-04-13 22:30:51 -0400

    Wrapped up the Name Field

--- source/components/NameField.jsx
+++ source/components/NameField.jsx
@@ -1,14 +1,22 @@
 var React = require('react');
 
 var NameField = react.createClass({
+      getInitialState: function() {
+          return {value: ""}; 
+      },
 
       onChange: function(e) {
-
+          this.setState({value: e.target.value});
       },
 
       render: function() {
           return (
-               <input className="form-control" placeholder={this.props.type + " Name"} onChange={this.onChange} />
+               <input
+               className="form-control"
+               placeholder={this.props.type + " Name"}
+               onChange={this.onChange}
+               value={this.state.value} />
+
           );
       }
 });
