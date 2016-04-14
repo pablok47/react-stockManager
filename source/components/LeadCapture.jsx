874827e41443eb8a820c0785f622f6cd6cf2019a
pablok47 <pkaparis@gmail.com>
2016-04-13 22:44:55 -0400

    Imported the Email Field and Name Field to the LeadCapture.jsx file

--- source/components/LeadCapture.jsx
+++ source/components/LeadCapture.jsx
@@ -1,12 +1,21 @@
 var React = require('react');
+var EmailField = require('./EmailField.jsx');
+var NameField = require('./NameField.jsx');
 
 var LeadCapture = React.createClass({
+
+    onSubmit: function(e) {
+          
+    },
+
     render: function() {
           return (
               <div className="col-sm-3">
                   <div className="panel panel-default">
                       <div className="panel-body">
-
+                          <NameField type="First" />
+                          <EmailField />
+                          <button className="btn btn-primary" onClick={this.onSubmit}>Submit</button>
                       </div>
                   </div>
               </div>
